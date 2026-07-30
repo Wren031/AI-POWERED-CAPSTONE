720cfc31217e9d9ba510ae9eb81da7e04df0e781
WREN MONTERO JAVIER <javierrenren1@gmail.com>
2026-07-31 06:38:23 +0800

    sam

--- src/features/reports/page/ReportPage.tsx
+++ src/features/reports/page/ReportPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import type { CSSProperties } from "react";
 import { 
   Users, Calendar, ShieldCheck, FileDown, 
